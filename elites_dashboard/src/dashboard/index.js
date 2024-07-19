c0d27e781bc097d36a9159be24d122a509e3630c
Caren Chepngetich <chepngetichkosgeicarenn@gmail.com>
2024-07-19 08:40:50 +0300

    made changes

---
+++ elites_dashboard/src/dashboard/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const Dashboard=()=>{
+    return(
+        <div>
+            <h2>Dashboard</h2>
+            <div>
+                <img></img>
+                <img></img>
+                <img></img>
+            </div>
+        </div>
+    )
+}
+export default Dashboard
---
+++ elites_dashboard/src/products/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const Products = ()=>{
+    return(
+        <div>
+            <h2>Product Updates</h2>
+            <div>
+            <img src="fertilizer3.jpg"/>
+            <img src="fertilizer5.jpg"/>
+            <img src="fertilizer1.jpg"/>
+            </div>
+        </div>
+    )
+}
+export default Products
